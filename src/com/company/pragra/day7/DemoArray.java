f7e25ab2a4c3a5c6f0368e288b484a0be7817d76
jismimathew <jismimathew18@gmail.com>
2022-11-03 19:50:39 -0400

    initialchange

---
+++ src/com/company/pragra/day7/DemoArray.java
@@ -0,0 +1,11 @@
+package com.company.pragra.day7;
+
+public class DemoArray {
+    public static void main(String[] args) {
+        int arr[]= new int[10];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=i+1;
+        }
+        System.out.println(arr[9]);
+    }
+}
