bdb341f22c225aecbbee2ef1c5b6056c266132a2
jismimathew <jismimathew18@gmail.com>
2022-11-06 23:18:17 -0500

    latest changes

--- src/com/company/pragra/day5/Friend.java
+++ src/com/company/pragra/day5/Friend.java
@@ -3,5 +3,6 @@ package com.company.pragra.day5;
 public class Friend {
     public static void main(String[] args) {
         System.out.println("hi friend");
+        System.out.println("Jismi");
     }
 }
